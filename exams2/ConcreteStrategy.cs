f7683958794e62faad6f71fe13f8a99837a8f59e
krm.clk.1903@gmail.com <krm.clk.1903@gmail.com>
2021-11-02 23:54:30 +0300

    Add project files.

---
+++ exams2/ConcreteStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exams2
+{
+    public class QuickSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            list.Sort();  // Default is Quicksort
+            Console.WriteLine("QuickSorted list ");
+        }
+    }
+    
+    public class ShellSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.ShellSort();  not-implemented
+            Console.WriteLine("ShellSorted list ");
+        }
+    }
+   
+    public class MergeSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.MergeSort(); not-implemented
+            Console.WriteLine("MergeSorted list ");
+        }
+    }
+}
---
+++ exams2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exams2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            SortedList studentRecords = new SortedList();
+            studentRecords.Add("Tomy");
+            studentRecords.Add("Duffy");
+            studentRecords.Add("Carlos");
+            studentRecords.Add("Alex");
+            studentRecords.Add("Anna");
+            studentRecords.SetSortStrategy(new QuickSort());
+            studentRecords.Sort();
+            studentRecords.SetSortStrategy(new ShellSort());
+            studentRecords.Sort();
+            studentRecords.SetSortStrategy(new MergeSort());
+            studentRecords.Sort();
+            
+            Console.ReadKey();
+        }
+    }
+
+}
